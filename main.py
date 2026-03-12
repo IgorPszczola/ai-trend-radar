1b8ed86bc745a9075347d4a1cd8b9930bf4b148b
igorpszczola1 <251208@edu.p.lodz.pl>
2026-03-12 15:16:54 +0100

    Dodanie endpointu do pobrania danych z twittera

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
+import httpx 
 
 app = FastAPI()
 
-@app.get("/api/health")
-async def check_health():
-    return {"status": "ok",
-            "message": "API is healthy"}
+
+@app.get("/api/trends/reddit")
+async def fetch_reddit_trends():
+    url = "https://www.reddit.com/r/streetwear/top.json?limit=30&t=week"
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
+    
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        data = response.json()
+    
+    trends = []
+    for post in data["data"]["children"]:
+        if len(post["data"]["title"]) > 3:
+            trends.append({
+                "title": post["data"]["title"],
+                "url": "https://reddit.com" + post["data"]["permalink"],
+                "score": post["data"]["score"]
+            })
+        else:
+            continue
+    
+    return { 
+        "source": "r/streetwear",
+        "trends": trends
+    }
